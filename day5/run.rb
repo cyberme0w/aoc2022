ec09a007d2dfddd99aaa1b146a1fb75bcd33c842
cyberme0w <iuri_archer@hotmail.com>
2022-12-05 21:04:01 +0100

    Day5: dynamically generate initial stack states

--- day5/run.rb
+++ day5/run.rb
@@ -11,23 +11,40 @@
 # [S] [N] [F] [G] [W] [B] [H] [F] [N]
 #  1   2   3   4   5   6   7   8   9
 
-stacks = [
-  %w[S Z P D L B F C],
-  %w[N V G P H W B],
-  %w[F W B J G],
-  %w[G J N F L W C S],
-  %w[W J L T P M S H],
-  %w[B C W G F S],
-  %w[H T P M Q B W],
-  %w[F S W T],
-  %w[N C R]
-]
+# Figure out how many stacks we have
+total_stacks = 0
+movement_start_line = 0
+
+input = File.readlines(ARGV[0])
+input.each_with_index do |l, index|
+  next unless l =~ /#(\s+(\d+)){1,}/
+
+  movement_start_line = index + 3
+  total_stacks = Regexp.last_match(2).to_i
+end
+
+# Initialize the stacks with a nil, which we will remove in the next step
+stacks = []
+(1..total_stacks).each { stacks << [] }
+puts "The crane is handling #{total_stacks} stacks. The first movement command is on line #{movement_start_line}."
+
+stack_input = input[0, movement_start_line - 3].reverse
+movement_input = input[movement_start_line - 1, input.size]
+
+# Process the initial stack states
+stack_input.each do |l|
+  l.chars.each_with_index do |c, index|
+    next unless c =~ /[A-Z]/
+
+    stacks[(index - 3) / 4] << c
+  end
+end
 
 # For part 2, set to false
-part1 = false
+part1 = true
 
 # Read in and process each line
-File.foreach(ARGV[0]) do |line|
+movement_input.each do |line|
   next unless line =~ /^move (\d+) from (\d+) to (\d+)$/
 
   qty = Regexp.last_match(1).to_i
